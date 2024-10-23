6bc571e759feef6e7bf3225d61a4f0c094bb3a80
samuel0953 <samibelachew56@gmail.com>
2024-10-23 08:35:52 -0700

    third

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello sami");
+let apiQuotes = [];
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = response.json();
+    console.log(apiQuotes);
+  } catch {
+    console.log("Error fetching quotes");
+  }
+}
+getQuotes();
